46f694370ef2c1f18116a6e7fec5464b1598ca03
benjamin <benjbruant@gmail.com>
2024-08-16 21:39:11 +0200

    bullet points actifs (reste le changement de background à réaliser)

--- assets/script.js
+++ assets/script.js
@@ -23,13 +23,36 @@ const leftArrow = document.querySelector('.arrow_left');
 const rightArrow = document.querySelector('.arrow_right');
 const bannerImage = document.querySelector('.banner-img');
 const bannerText = document.querySelector('#banner p');
-const spanDot = document.querySelector('.dot');
+const spanDot = document.getElementsByClassName("dot");
 
-spanDot.addEventListener('click',()=>{
+/* let i = 0 */
+spanDot[0].addEventListener('click',()=>{
 	console.log(spanDot)
-	console.log('Ça marche !!! YOUPI !!! Tralala !!!')
+	console.log('Ça marche !!! One!')
+	slideOn = "./assets/images/slideshow/" + slides[0].image;
+	bannerImage.setAttribute("src",slideOn);
+		
  })
-
+ spanDot[1].addEventListener('click',()=>{
+	console.log(spanDot)
+	console.log('Ça marche !!! Two!')
+	slideOn = "./assets/images/slideshow/" + slides[1].image;
+	bannerImage.setAttribute("src",slideOn);
+		
+ })
+ spanDot[2].addEventListener('click',()=>{
+	console.log(spanDot)
+	console.log('Ça marche !!! Three!')
+	slideOn = "./assets/images/slideshow/" + slides[2].image;
+	bannerImage.setAttribute("src",slideOn);
+ })		
+ spanDot[3].addEventListener('click',()=>{
+	console.log(spanDot)
+	console.log('Ça marche !!! Four!')
+	slideOn = "./assets/images/slideshow/" + slides[3].image;
+	bannerImage.setAttribute("src",slideOn);
+		
+ })
 let indexForSlides = 0;
 let slideOn
 
@@ -65,58 +88,3 @@ function dotSlides() {
  console.log(indexForSlides);
 
 
-/* console.log (indexForSlides)
-if(indexForSlides > slides.length -1) {
-	slideOn = "./assets/images/slideshow/" + slides[0].image;
-}  */
-/* rightArrow.addEventListener('click', ()=>{
-	
-	if(indexForSlides > slides.length -1) {
-		indexForSlides = 0;
-	} else {
-	indexForSlides = indexForSlides + 1;
-	let slideOn = "./assets/images/slideshow/" + slides[indexForSlides].image
-	bannerImage.setAttribute("src",slideOn)
-	}
-	
-	console.log(slides[indexForSlides] .image );
-}) */
-
-/*Test du scope de la fonction */
-/* function toward(){
-	let i = 0
-	indexForSlides = slides[i]
-	return indexForSlides
-}
-toward()
-console.log (indexForSlides) */
-
-/* Mise en place des fonctions du slider*/
-/* rightArrow.addEventListener('click', ()=>{
-	/* console.log('OK right'); */
-	/* bannerImage.setAttribute("src","./assets/images/slideshow/slide2.jpg") 
-	bannerImage.setAttribute("src",slideOn)
-}) */
-/* let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let dots = document.getElementsByClassName("dot");
-   
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" dot_selected", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " dot_selected";
-} */
