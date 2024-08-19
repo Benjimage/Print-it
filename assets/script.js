3ef390c20a5ce0eb0733773f6e852f8259c67ee4
benjamin <benjbruant@gmail.com>
2024-08-19 10:41:13 +0200

    mise en place des fonctions de défilement et défilement infini

--- assets/script.js
+++ assets/script.js
@@ -18,69 +18,78 @@ const slides = [
 	}
 ]
  
-
 const leftArrow = document.querySelector('.arrow_left');
 const rightArrow = document.querySelector('.arrow_right');
 const bannerImage = document.querySelector('.banner-img');
 const bannerText = document.querySelector('#banner p');
 const spanDot = document.getElementsByClassName("dot");
 
-spanDot[0].addEventListener('click',()=>{	
-	slideOn = "./assets/images/slideshow/" + slides[0].image;
-	bannerImage.setAttribute("src",slideOn);
-	console.log('Point 1')
- })
-
- spanDot[1].addEventListener('click',()=>{
-	slideOn = "./assets/images/slideshow/" + slides[1].image;
-	bannerImage.setAttribute("src",slideOn);
-	console.log('Point 2')
- })
-
- spanDot[2].addEventListener('click',()=>{
-	slideOn = "./assets/images/slideshow/" + slides[2].image;
-	bannerImage.setAttribute("src",slideOn);
-	console.log('Point 3')
- })	
-
- spanDot[3].addEventListener('click',()=>{
-	slideOn = "./assets/images/slideshow/" + slides[3].image;
-	bannerImage.setAttribute("src",slideOn);
-	console.log('Point 4')
- })
-
 let indexForSlides = 0;
 let slideOn
+let scrollingTime
+let restartTime
 
-function dotSlides() {
-    let i;
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" dot_selected", "");
-    }
-    
-    dots[indexForSlides].className += " dot_selected";
-} 
+autoSlider()
+
+leftArrow.addEventListener('click', previousSlide)
+rightArrow.addEventListener('click', nextSlide)
+rightArrow.addEventListener('click', pauseSlider)
+
+function autoSlider(){
+	 scrollingTime = setInterval(nextSlide,3000)
+}
+function pauseSlider(){
+	clearInterval(scrollingTime)
+}
 
 
- leftArrow.addEventListener('click', ()=>{
-
+function nextSlide() {
+	indexForSlides++;
+	if (indexForSlides > slides.length -1){indexForSlides = 0}
+		slideOn = "./assets/images/slideshow/" + slides[indexForSlides].image;
+		bannerImage.setAttribute("src",slideOn);
+		dotSlides()
+	} 
+function previousSlide(){
 		indexForSlides--;
 		if (indexForSlides < 0) {indexForSlides = slides.length -1}
 		slideOn = "./assets/images/slideshow/" + slides[indexForSlides].image;
 		bannerImage.setAttribute("src",slideOn);
 		dotSlides()
-		console.log("<<Left")
- })
-
- rightArrow.addEventListener('click', ()=>{
-
-		indexForSlides++;
-		if (indexForSlides > slides.length -1){indexForSlides = 0}
-		slideOn = "./assets/images/slideshow/" + slides[indexForSlides].image;
+		pauseSlider()
+	}
+	
+	spanDot[0].addEventListener('click',()=>{	
+		slideOn = "./assets/images/slideshow/" + slides[0].image;
 		bannerImage.setAttribute("src",slideOn);
-		dotSlides()
-		console.log("Right>>")
- })
-
+		console.log('Point 1')
+	 })
+	
+	 spanDot[1].addEventListener('click',()=>{
+		slideOn = "./assets/images/slideshow/" + slides[1].image;
+		bannerImage.setAttribute("src",slideOn);
+		console.log('Point 2')
+	 })
+	
+	 spanDot[2].addEventListener('click',()=>{
+		slideOn = "./assets/images/slideshow/" + slides[2].image;
+		bannerImage.setAttribute("src",slideOn);
+		console.log('Point 3')
+	 })	
+	
+	 spanDot[3].addEventListener('click',()=>{
+		slideOn = "./assets/images/slideshow/" + slides[3].image;
+		bannerImage.setAttribute("src",slideOn);
+		console.log('Point 4')
+	 })
+	
+	
+	function dotSlides() {
+		let i;
+		let dots = document.getElementsByClassName("dot");
+		for (i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace(" dot_selected", "");
+		}
+		dots[indexForSlides].className += " dot_selected";
+	} 
 
